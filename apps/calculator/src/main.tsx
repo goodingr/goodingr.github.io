db8af839b46cc93fd90e4df187d7d1caabb197df
Robert Gooding <goodingr@users.noreply.github.com>
2025-11-09 13:46:37 -0500

    did nothing

--- apps/calculator/src/main.tsx
+++ apps/calculator/src/main.tsx
@@ -5,5 +5,6 @@ import { CalculatorApp } from '@portfolio/calculator-widget'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CalculatorApp />
+    
   </React.StrictMode>
 )
